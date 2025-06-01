ca79af7bc111116b85cbced7a36bc54bd0058aff
amgenious <henrytweneboah956@gmail.com>
2025-06-01 09:27:09 +0000

    updated the cron job

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import { initDB } from './config/db.js'
 import rateLimiter from './middleware/rateLimiter.js'
 import transactionsRoute from "./routes/transactionsRoute.js"
+import job from "./config/cron.js"
 dotenv.config()
 
 const app = express()
